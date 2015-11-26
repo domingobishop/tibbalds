322ae7060b2fbfbd51874235b2ae02252013e010
Christopher Bishop <domingobishop@gmail.com>
2015-11-26 17:54:37 +0800

    created our team template

---
+++ page-ourteam.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Template Name: Our team
+ *
+ */
+get_header(); ?>
+
+    <div id="content" class="page-intro color-red-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="title-header">
+                        <?php if (is_home() && get_option('page_for_posts')) { ?>
+                            <h1><?php echo apply_filters('the_title', get_page(get_option('page_for_posts'))->post_title); ?></h1>
+                        <?php } elseif (is_singular()) { ?>
+                            <h1><?php the_title(); ?></h1>
+                            <?php while (have_posts()) : the_post(); ?>
+                                <div class="entry-content">
+                                    <?php the_content(); ?>
+                                </div>
+                            <?php endwhile; ?>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+            <div class="row bc-post-loop-wrap">
+                <?php query_posts('category_name=' . get_the_title() . '&post_status=publish,future'); ?>
+                <?php if (have_posts()) : ?>
+                    <?php /* The loop */ ?>
+                    <?php while (have_posts()) : the_post(); ?>
+
+                            <div class="col-md-2 col-lg-2">
+                                <div id="post-<?php the_ID(); ?>">
+                                <?php if (has_post_thumbnail() && !post_password_required() && !is_attachment()) : ?>
+                                            <?php the_post_thumbnail('full', array('class' => 'img-responsive')); ?>
+                                <?php endif; ?>
+                                <h4 class="entry-title">
+                                        <?php the_title(); ?>
+                                </h4>
+                                </div>
+                            </div>
+
+                    <?php endwhile; ?>
+                <?php else : ?>
+                    <h2>No content</h2>
+                <?php endif; ?>
+                <nav>
+                    <ul>
+                        <li class="previous"><?php next_posts_link(__('&#8249; Older posts', 'blankcanvas')); ?></li>
+                        <li class="next"><?php previous_posts_link(__('Newer posts &#8250;', 'blankcanvas')); ?></li>
+                    </ul>
+                    <!-- end of .navigation -->
+                </nav>
+                <!-- #post-loop -->
+            </div>
+        </div>
+    </div>
+    <!-- #content -->
+
+<?php get_footer(); ?>

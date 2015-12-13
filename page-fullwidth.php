6285be9f4dc1646b9cb1db2ad23e49d0639b9edc
Christopher Bishop <domingobishop@gmail.com>
2015-12-14 09:55:41 +1100

    moved page intro into content area

--- page-fullwidth.php
+++ page-fullwidth.php
@@ -9,33 +9,31 @@ get_header(); ?>
         <?php $bcimage = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail'); ?>
     <?php endif; ?>
 
-    <div id="page-intro" class="page-intro">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-lg-6">
-                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                        <div class="title-header">
-                            <h1>
-                                <?php the_title(); ?>
-                            </h1>
-                        </div>
-                        <div class="excerpt-content">
-                            <?php if ($post->post_excerpt) the_excerpt(); ?>
-                        </div>
-                        <!-- .excerpt-content -->
 
-                    </article>
-                </div>
-                <div class="col-md-6 col-lg-6">
-                    <?php if ($bcimage) {
-                        echo '<img src="' . $bcimage[0] . '" class="img-responsive">';
-                    } ?>
-                </div>
+    <div id="content" class="bc-content page">
+    <div class="container">
+        <div class="row">
+            <div class="page-intro col-md-6 col-lg-6">
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <div class="title-header">
+                        <h1>
+                            <?php the_title(); ?>
+                        </h1>
+                    </div>
+                    <div class="excerpt-content">
+                        <?php if ($post->post_excerpt) the_excerpt(); ?>
+                    </div>
+                    <!-- .excerpt-content -->
+
+                </article>
+            </div>
+            <div class="col-md-6 col-lg-6">
+                <?php if ($bcimage) {
+                    echo '<img src="' . $bcimage[0] . '" class="img-responsive">';
+                } ?>
             </div>
         </div>
     </div>
-
-    <div id="content" class="bc-content page">
     <div class="container">
     <div class="row">
     <div class="col-lg-12">
--- page.php
+++ page.php
@@ -4,26 +4,23 @@
         <?php $bcimage = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail'); ?>
     <?php endif; ?>
 
-    <div id="page-intro" class="page-intro">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                        <div class="title-header">
-                            <h1>
-                                <?php the_title(); ?>
-                            </h1>
-                        </div>
-                        <div class="excerpt-content">
-                            <?php if ($post->post_excerpt) the_excerpt(); ?>
-                        </div>
-                    </article>
-                </div>
+    <div id="content" class="bc-content page">
+    <div class="container">
+        <div class="row">
+            <div class="page-intro col-lg-12">
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <div class="title-header">
+                        <h1>
+                            <?php the_title(); ?>
+                        </h1>
+                    </div>
+                    <div class="excerpt-content">
+                        <?php if ($post->post_excerpt) the_excerpt(); ?>
+                    </div>
+                </article>
             </div>
         </div>
     </div>
-
-    <div id="content" class="bc-content page">
     <div class="container">
     <div class="row">
     <div class="col-md-6 col-lg-6">
